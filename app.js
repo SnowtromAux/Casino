345cb727cfd5f7f4aa9c7cef9e70727e9a9d4dd9
Alex Dimitrov <ad@auxnederlandbv.nl>
2022-11-05 18:22:13 +0200

    adde node js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+	res.sendFile('casino.html', {root: __dirname});
+})
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`); 
+});
