280a2d3b12b83a9b7d7be16dbeb1c1658951b0a8
alex dimitrov <ad@auxcode.com>
2022-11-15 00:28:48 +0200

    casino

---
+++ public/aviator/aviator.js
@@ -0,0 +1,5 @@
+let socket = io();
+
+function multiplierCalculation(){
+    
+}
--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 let menu = "start";
 
+//Game 1
 function start_super_hot(){
 	menu = "super_hot";
 	document.getElementsByClassName("super_hot_game")[0].style.display = "block";
@@ -10,4 +11,13 @@ function stop_super_hot(){
 	menu = "start";
 	document.getElementsByClassName("super_hot_game")[0].style.display = "none";
 	document.getElementsByClassName("start_menu")[0].style.display = "block";	
+}
+
+
+
+//Game 2
+function start_aviator(){
+	menu = "aviator";
+	document.getElementsByClassName("aviator")[0].style.display = "block";
+	document.getElementsByClassName("start_menu")[0].style.display = "none";
 }
