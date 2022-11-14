1d33cd5952057c109799f38f3bcc2afef2c7db3e
alex dimitrov <ad@auxcode.com>
2022-11-15 00:29:16 +0200

    xd

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 const port = 4000;
 
 app.get('/', (req, res) => {
 	res.sendFile('casino.html', {root: __dirname});
 })
 
+io.on('connection', function(socket){
+    console.log("niakoi se svurza");
+  
+});
+
 app.use(express.static("public"));
 
-app.listen(port, () => {
+http.listen(port, () => {
     console.log(`Now listening on port ${port}`); 
 });
